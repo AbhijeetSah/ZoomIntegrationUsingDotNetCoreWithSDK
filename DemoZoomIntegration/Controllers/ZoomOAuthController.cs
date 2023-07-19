7c5ffa4a12a717dee7a43eb5c7f5114ae3250217
AbhijeetSah <abhijeet.sah5895@gmail.com>
2023-07-19 14:23:01 +0545

    changes with zoom OAUTH TOKEN AND GENERATE MEETING

---
+++ DemoZoomIntegration/Controllers/ZoomOAuthController.cs
@@ -0,0 +1,100 @@
+﻿using DemoZoomIntegration.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Text;
+
+namespace DemoZoomIntegration.Controllers
+{
+    public class ZoomOAuthController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ZoomOAuthController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            var clientId = "YOUR_CLIENT_ID";
+            var returnUrl = "https://localhost:7288/ZoomOAuth/ReturnUrl";
+            return Redirect($"https://zoom.us/oauth/authorize?response_type=code&client_id={clientId}&redirect_uri={Uri.EscapeDataString(returnUrl)}");
+        }
+        public async Task<IActionResult> ReturnUrl()
+        {
+            // Get the authorization code from the query string.
+            string code = Request.Query["code"];
+            // Exchange the authorization code for an access token.
+            var client = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://zoom.us/oauth/token");
+           
+            request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes("YOUR_CLIENT_ID:YOUR_CLIENT_SECRET")));
+            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "grant_type", "authorization_code" },
+                { "code", code },
+                { "redirect_uri", "https://localhost:7288/ZoomOAuth/ReturnUrl" },
+            });
+            var response = await client.SendAsync(request);
+            var accessToken = response.Content.ReadAsStringAsync().Result;
+            var token= JsonConvert.DeserializeObject<ZoomOAuthToken>(accessToken);
+
+            // Save the access token in the user's session.
+            //HttpContext.Session.SetString("AccessToken", accessToken);
+
+            // Redirect the user to the home page.
+
+            RestClient rclient = new RestClient("https://api.zoom.us/v2");
+            rclient.AddDefaultHeader("Authorization", $"Bearer {token.access_token}");
+
+            RestRequest rrequest = new RestRequest("/users/me/meetings", Method.Post);
+            rrequest.AddJsonBody(new
+            {
+                topic = "My Meeting",
+                type = 1,
+                duration = 60
+            });
+
+
+
+            //{
+            //    topic: "Beema Pradhikaran",
+            //      type:2,
+            //      start_time: "2023-07-17T15:25:10Z",
+            //      duration:"3",
+            //      settings:{
+            //                host_video:true,
+            //                participant_video:true,
+            //                join_before_host:false,
+            //                mute_upon_entry:"true",
+            //                watermark: "true",
+            //                audio: "voip",
+            //                auto_recording: "cloud"
+            //         }
+
+            //}
+
+
+            var rresponse = await rclient.ExecuteAsync(rrequest);
+
+            if (rresponse.IsSuccessful)
+            {
+                // Meeting created successfully
+                var result = rresponse.Content;
+                var createdMeeting = JsonConvert.DeserializeObject<ZoomOAuthModel>(result);
+                // Process the created meeting details as needed
+
+                return View();
+            }
+            else
+            {
+                // Error occurred while creating the meeting
+                return View("Error");
+            }
+
+        }
+        
+    }
+}
---
+++ DemoZoomIntegration/Models/ZoomOAuthModel.cs
@@ -0,0 +1,89 @@
+﻿namespace DemoZoomIntegration.Models
+{
+    public class ZoomOAuthModel
+    {
+    }
+
+    public class ZoomUserData
+    {
+        public string? uuid { get; set; }
+        public string? id { get; set; }
+        public string? host_id { get; set; }
+        public string? host_email { get; set; }
+        public string? topic { get; set; }
+        public string? type { get; set; }
+        public string? status { get; set; }
+        public string? start_time { get; set; }
+        public int? duration { get; set; }
+        public string? timezone { get; set; }
+        public string? created_at { get; set; }
+        public string? start_url { get; set; }
+        public string? join_url { get; set; }
+        public string? password { get; set; }
+        public string? h323_password { get; set; }
+        public string? pstn_password { get; set; }
+        public string? encrypted_password { get; set; }
+        public SettingsModel? settings { get; set; }
+        public bool? pre_schedule { get; set; }
+
+    }
+
+    public class SettingsModel
+    {
+        public bool? host_video { get; set; }
+        public bool? participant_video { get; set; }
+        public bool? cn_meeting { get; set; }
+        public bool? in_meeting { get; set; }
+        public bool? join_before_host { get; set; }
+        public int? jbh_time { get; set; }
+        public bool? mute_upon_entry { get; set; }
+        public bool? watermark { get; set; }
+        public bool? use_pmi { get; set; }
+        public int? approval_type { get; set; }
+        public bool? audio { get; set; }
+        public bool? auto_recording { get; set; }
+        public bool? enforce_login { get; set; }
+        public string? enforce_login_domains { get; set; }
+        public string? alternative_hosts { get; set; }
+        public bool? alternative_host_update_polls { get; set; }
+        public bool? close_registration { get; set; }
+        public bool? show_share_button { get; set; }
+        public bool? allow_multiple_devices { get; set; }
+        public bool? registrants_confirmation_email { get; set; }
+        public bool? waiting_room { get; set; }
+        public bool? request_permission_to_unmute_participants { get; set; }
+        public bool? meeting_authentication { get; set; }
+        public string? encryption_type { get; set; }
+        public bool? alternative_hosts_email_notification { get; set; }
+        public bool? show_join_info { get; set; }
+        public bool? device_testing { get; set; }
+        public bool? focus_mode { get; set; }
+        public bool? enable_dedicated_group_chat { get; set; }
+        public bool? private_meeting { get; set; }
+        public bool? email_notification { get; set; }
+        public bool? host_save_video_order { get; set; }
+        public bool? email_in_attendee_report { get; set; }
+        //"approved_or_denied_countries_or_regions": {
+        //    "enable": false
+        //},
+        //"question_and_answer": {
+        //    "enable": false
+        //},
+        //"breakout_room": {
+        //    "enable": false
+        //},
+        //    "sign_language_interpretation": {
+        //    "enable": false
+        //},
+    }
+
+    public class ZoomOAuthToken
+    {
+        public string? access_token { get; set; }
+        public string? token_type { get; set; }
+        public string? refresh_token { get; set; }
+        public int? expires_in { get; set; }
+
+    }
+
+}
